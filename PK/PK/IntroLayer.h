dae569ac56a815441c8e58d31b7c5dc8fc02dd96
fiicere <fiicere@gmail.com>
2013-07-01 15:36:32 -0400

    New Project

---
+++ PK/PK/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  PK
+//
+//  Created by Kevin Yue on 7/1/13.
+//  Copyright Kevin Yue 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ PK/PK/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PK
+//
+//  Created by Kevin Yue on 7/1/13.
+//  Copyright Kevin Yue 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
