e1e1b15a37808e3299e6e13d88062b9c1717ce85
fiicere <fiicere@gmail.com>
2013-07-02 14:40:35 -0400

    projectile class

---
+++ PK/PK/Projectile.h
@@ -0,0 +1,13 @@
+//
+//  Projectile.h
+//  PK
+//
+//  Created by Kevin Yue on 7/2/13.
+//  Copyright (c) 2013 Kevin Yue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Projectile : NSObject
+
+@end
---
+++ PK/PK/Projectile.m
@@ -0,0 +1,13 @@
+//
+//  Projectile.m
+//  PK
+//
+//  Created by Kevin Yue on 7/2/13.
+//  Copyright (c) 2013 Kevin Yue. All rights reserved.
+//
+
+#import "Projectile.h"
+
+@implementation Projectile
+
+@end
